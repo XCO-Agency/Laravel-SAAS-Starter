d78ba131944d8340c5de1b8a1498d3e58723ed7c
ANASS EZ-ZOUAINE <ans-ezzouaine@hotmail.com>
2025-12-03 23:53:11 +0100

    Add notification language file for workspace invitations
    - Created a new language file for notifications related to workspace invitations.
    - Included subject, greeting, and action messages for user invitations.
    - Provided instructions for accepting invitations with existing or new accounts.
    - Added expiration notice and option to ignore the invitation.

---
+++ lang/en/notifications.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'invitation' => [
+        'subject' => 'You have been invited to join :workspace',
+        'greeting' => 'Hello!',
+        'intro' => 'You have been invited to join :workspace as a :role.',
+        'existing_account' => 'You can accept this invitation using your existing account with :email.',
+        'new_account' => 'You will need to create an account to accept this invitation.',
+        'action' => 'Accept Invitation',
+        'expires' => 'This invitation will expire on :date.',
+        'ignore' => 'If you did not expect this invitation, you can safely ignore this email.',
+    ],
+];
+
